223c71553a3a49a55c5634bd4b6571ba9433ff03
cocoPastell <schmutz.carole@gmail.com>
2023-04-05 22:43:30 +0000

    my second commit

--- counter/testcounter.js
+++ counter/testcounter.js
@@ -1,18 +1,48 @@
-let arrayOutput = [4, 7, 2];
+//version 1
 let arraySet1 = [3, 1, 7, 9];
 let arraySet2 = [2, 4, 1, 9, 3];
-let counter = 0;
+let counter = true;
+let output = 0;
 
-for (let i = 0; i < arrayOutput.length; i++) {
+for (let i = 0; i < arraySet2.length; i++) {
   for (let j = 0; j < arraySet1.length; j++) {
-    if (arrayOutput[i] == arraySet1[j]) {
-      counter += arrayOutput[i];
+    if (arraySet2[i] == arraySet1[j]) {
+      counter = false;
     }
   }
-  for (let k = 0; k < arraySet2.length; k++) {
-    if (arrayOutput[i] == arraySet2[k]) {
-      counter += arrayOutput[i];
+  if (counter == true) {
+    output += arraySet2[i];
+  }
+  counter = true;
+}
+for (let i = 0; i < arraySet1.length; i++) {
+  for (let j = 0; j < arraySet2.length; j++) {
+    if (arraySet1[i] == arraySet2[j]) {
+      counter = false;
     }
+  }
+  if (counter == true) {
+    output += arraySet1[i];
+  }
+  counter = true;
+}
+console.log(output);
+
+//version 2
+let arrSet1 = [3, 1, 7, 9];
+let arrSet2 = [2, 4, 1, 9, 3];
+let result = 0;
+
+for (let i = 0; i < arrSet1.length; i++) {
+  if (!arrSet2.includes(arrSet1[i])) {
+    result += arrSet1[i];
   }
 }
-console.log(counter);
+
+for (let i = 0; i < arrSet2.length; i++) {
+  if (!arrSet1.includes(arrSet2[i])) {
+    result += arrSet2[i];
+  }
+}
+
+console.log(result);
